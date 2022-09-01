4e47af5e03557505ded3bd086c34d7304c8b7705
Snickbit <snickbit@users.noreply.github.com>
2022-09-01 00:10:27 -0400

    fix!: switch to a single parameter instead of an object parameter
    BREAKING-CHANGE: ansiRegex now takes a single boolean parameter

--- src/ansi-regex.ts
+++ src/ansi-regex.ts
@@ -3,7 +3,7 @@
  * Ansi regular expression
  * @param onlyFirst
  */
-export function ansiRegex({onlyFirst = false} = {}): RegExp {
+export function ansiRegex(onlyFirst = false): RegExp {
 	const pattern = [
 		'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
 		'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))'
